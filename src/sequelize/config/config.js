5da93533a55e62d272084acf3f4059b95d946a85
brunoro7 <bruno.ro7c@gmail.com>
2022-07-25 02:47:51 -0300

    feat: Criando as configuração do banco de dados, junto ao Supabase;

--- src/sequelize/config/config.js
+++ src/sequelize/config/config.js
@@ -1,23 +1,30 @@
+require('dotenv/config');
+
+const { HOST, PASSWORD_POSTGRES, DATABASE, DB_USERNAME, DB_PORT } = process.env;
+
 module.exports = {
   development: {
-    username: 'root',
-    password: null,
-    database: 'database_development',
-    host: '127.0.0.1',
+    username: DB_USERNAME || 'root',
+    password: PASSWORD_POSTGRES || null,
+    database: DATABASE || 'database_development',
+    host: HOST || '127.0.0.1',
+    port: DB_PORT,
     dialect: 'postgres',
   },
   test: {
-    username: 'root',
-    password: null,
-    database: 'database_test',
-    host: '127.0.0.1',
+    username: DB_USERNAME || 'root',
+    password: PASSWORD_POSTGRES || null,
+    database: DATABASE || 'database_test',
+    host: HOST || '127.0.0.1',
+    port: DB_PORT,
     dialect: 'postgres',
   },
   production: {
-    username: 'root',
-    password: null,
-    database: 'database_test',
-    host: '127.0.0.1',
+    username: DB_USERNAME || 'root',
+    password: PASSWORD_POSTGRES || null,
+    database: DATABASE || 'database_test',
+    host: HOST || '127.0.0.1',
+    port: DB_PORT,
     dialect: 'postgres',
   },
 };
